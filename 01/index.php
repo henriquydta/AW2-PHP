15f969e5e5f184ed3b6bed152f3525c2b805fe7f
henry <127354681+henriquydta@users.noreply.github.com>
2025-08-20 14:28:16 -0300

    Create index.php

---
+++ 01/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP - Aula 1</title>
+  </head>
+  <body>
+    <form action="pagina2.php" method="post">
+      <input type="number" name="numero_min" placeholder="Digite um número">
+      <input type="number" name="numero_max" placeholder="Digite outro número">
+      <input type="submit">
+    </form> 
+    <br>
+    <form action="pagina3.php" method="post">
+      <input type="text" name="frase" placeholder="Digite uma frase">
+      <input type="submit">
+    </form>
+  </body>
+</html>
